462dec39244c2e70de35b899f3c9758cff78fb49
Manasa Patra <220332839+manasaa-18@users.noreply.github.com>
2026-07-02 18:32:38 +0530

    Time: 3 ms (55.99%), Space: 13.2 MB (53.27%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[nums[i]] = i       
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
